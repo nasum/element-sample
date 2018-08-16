b69637d0d9c237e6022439b672320a2e57f25888
nasum <nasu@scouty.co.jp>
2018-08-16 19:06:51 +0900

    create sample

---
+++ element-sample_01/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="6">hoge</el-col>
+      <el-col :span="6">hoge</el-col>
+      <el-col :span="6">hoge</el-col>
+      <el-col :span="6">hoge</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+.el-col {
+  background-color: gray;
+}
+</style>
+
+
